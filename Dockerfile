9914572dcfa3a4488ad64a90142de62db60add9c
John Edward Whitley IV <john.whitley@mckesson.com>
2018-03-12 12:41:45 -0400

    Adding http.conf along w/ logic in http.sh

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,7 @@
 FROM httpd:latest
 MAINTAINER John Whitley
-COPY index.html /usr/local/apache2/htdocs/
+COPY index.html /usr/local/apache2/htdocs/index.html
+COPY httpd.conf /usr/local/apache2/conf/httpd.conf
 
 #Expose Ports
 EXPOSE 9010
--- httpd.sh
+++ httpd.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-sudo docker build -t my-httpd ~/httpd-hello-world
-sudo docker run --name httpd -d -p 9010:9010 my-httpd
+
+containercheck() {
+#Check for httpd
+  if [[ -n "$(sudo docker ps -q -f name=httpd)" ]]; then
+    echo "httpd Container is already running."
+  else
+    echo "Running clean up on httpd."
+    sudo docker rm prometheus
+    # Call to build and run container
+    echo "Building and starting httpd Container"
+    sudo docker build -t my-httpd ~/httpd-hello-world
+    sudo docker run --name httpd -d -p 9010:9010 my-httpd
+  fi
+}
+
+
+#Main
+containercheck
